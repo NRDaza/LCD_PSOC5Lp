7b02b3d065972d11c5014e8ea5713ed392ccc841
Nicolas Daza <nrodriguezd@correo.udistrital.edu.co>
2019-03-14 18:54:47 -0500

    first commit

---
+++ LCD.cydsn/codegentemp/LCD_u.sdc
@@ -0,0 +1,3 @@
+# Component constraints for C:\Users\jdrd3\Documents\PSoC Creator\Ejemplos\LCD.cydsn\TopDesign\TopDesign.cysch
+# Project: C:\Users\jdrd3\Documents\PSoC Creator\Ejemplos\LCD.cydsn\LCD.cyprj
+# Date: Tue, 22 Jan 2019 15:31:06 GMT
---
+++ LCD.cydsn/main.c
@@ -0,0 +1,33 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include "stdio.h"
+
+int main(void)
+{
+    LCD_Start();
+    LCD_Position(0,5);
+    LCD_PrintString("hola");
+    LCD_Position(1,5);
+    LCD_PrintString("prrosaaaaaaaaaaaaaaaaaaaa");
+    
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    for(;;)
+    {
+        /* Place your application code here. */
+    }
+}
+
+/* [] END OF FILE */
